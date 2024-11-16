49ffb5d1c6bd1acd9f36a013bd75f7972d00a51c
Avleen Kaur <avleen@Avleens-Air.attlocal.net>
2024-11-15 19:53:34 -0800

    next

--- main.cpp
+++ main.cpp
@@ -3,12 +3,15 @@
 
 #include <iostream>
 #include <deque>
+#include <cstdlib>
+#include <ctime>
 #include "Car.h"
 using namespace std;
 
 const int ISIZE = 2; //represents cars already in line 
 
 int main() {
+    srand(time(0));
 
     deque<Car> booth;
 
@@ -16,7 +19,20 @@ int main() {
         booth.push_back(Car()); //call Car -> calls constructor to generate random car
     }
 
-    for (int numcars = 0; numcars.)
+    cout << "Initial Queue:\n";
+    for(auto car : booth) {
+        cout << "\t";
+        car.print();
+    }
+
+    while (!booth.empty()) {
+        int prob = rand() % 100;
+
+        if (pro)
+
+
+    }
+
 
 
     return 0;
