57bd5c1446518c6da5b1cf27377eb3b8aeda390c
Avleen Kaur <avleen@Avleens-Air.attlocal.net>
2024-11-15 19:43:23 -0800

    second

--- main.cpp
+++ main.cpp
@@ -2,12 +2,21 @@
 //Lab 32: Toll Booth
 
 #include <iostream>
+#include <deque>
 #include "Car.h"
 using namespace std;
 
+const int ISIZE = 2; //represents cars already in line 
+
 int main() {
 
+    deque<Car> booth;
 
+    for (int i = 0; i < ISIZE; i++) {
+        booth.push_back(Car()); //call Car -> calls constructor to generate random car
+    }
+
+    for (int numcars = 0; numcars.)
 
 
     return 0;
